0bdd044eceb699b83129aeed73c2170ae72d6f0d
Atto Allas <atto.allas@gmail.com>
2023-09-15 23:26:49 -0400

    Openai embeddings added

--- data/gpt-encoding.py
+++ data/gpt-encoding.py
@@ -1,6 +1,22 @@
 import json
 
+import openai
+
+openai.api_key_path = "openai.key"
+
+def getEmbedding(text, model="text-embedding-ada-002"):
+   text = text.replace("\n", " ")
+   return openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
+
 with open("f23.json", "r") as f:
     semData = json.load(f)
 
+allCourseData = semData["courses"]
+for courseNum, courseData in allCourseData.items():
+    description = courseData["desc"]
+    descEmbedding = getEmbedding(description)
 
+    print(courseNum)
+    print(description)
+    print(descEmbedding)
+    break
