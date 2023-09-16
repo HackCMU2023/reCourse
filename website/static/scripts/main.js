f55ebf2eafdb64e64c0c32d684b1fc14a027f85a
lydia <llyang@andrew.cmu.edu>
2023-09-16 00:43:41 -0400

    Implement course list in website

--- website/static/scripts/main.js
+++ website/static/scripts/main.js
@@ -2,10 +2,26 @@ console.log("hola")
 document.getElementById("submit").addEventListener("click", onSubmit);
 
 function onSubmit(){
-    console.log("yay")
+    console.log(document.getElementById("courseSelect").value)
+    document.getElementById("output").innerHTML = "selected: " + document.getElementById("courseSelect").value
+}
+
+function loadCourses(){
+    document.getElementById("courseSelect").innerHTML = ""
+    var ajax_params = {
+        'url'     : "courseList",
+        'type'    : "get",
+        'success' : function(response){
+            for(var i=0; i<response.length; i++){
+                document.getElementById("courseSelect").innerHTML += "<option value=" + response[i] + ">" + response[i] + "</option>"
+            }
+        }
+    }
+    $.ajax( ajax_params )
 }
 
 $(document).ready(function () {
     //change selectboxes to selectize mode to be searchable
+    loadCourses()
     $("select").select2();
 });
